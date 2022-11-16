7f3bce591d639e9e94c62b59f79b9b41d688a6a2
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-16 20:56:06 +0800

    init app

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app_2/weather_app.dart';
+
+void main() => runApp(const WeatherApp());
---
+++ lib/weather_app.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Bloc Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
